934d4c3973ce35b38a37bc675988666bbc10df0e
Scott Wheeler <me@scottwheeler.ca>
2018-01-14 12:48:55 -0500

    writing views that actually do something issue #43

--- djangojourney/polls/views.py
+++ djangojourney/polls/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello world! You're at the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+## response containing links
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list
+#     }
+#     return HttpResponse(template.render(context, request))
+
+## response containing calculated text
+#     response = ", ".join([question.question_text for question in latest_question_list])
+#     return HttpResponse(response)
+
+## simple hello world
+#     return HttpResponse("Hello world! You're at the polls index")
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
