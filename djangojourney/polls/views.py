74446490cae59ea4da3332e7d22cd9baacbc3e08
Scott Wheeler <me@scottwheeler.ca>
2018-01-20 19:56:01 -0500

    Testing the DetailView #74

--- djangojourney/polls/views.py
+++ djangojourney/polls/views.py
@@ -56,6 +56,9 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 ## detail view as a function
 # def detail(request, question_id):
