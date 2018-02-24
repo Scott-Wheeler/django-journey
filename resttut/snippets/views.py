3b50b39f67a305fa8893c811b02c246bdf4150e6
Scott Wheeler <me@scottwheeler.ca>
2018-02-23 22:58:16 -0500

    Django REST - 4.2 - Adding endpoints for User models #138

--- resttut/snippets/views.py
+++ resttut/snippets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 from rest_framework import status, mixins, generics
 from rest_framework.decorators import api_view
@@ -10,7 +11,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 # Create your views here.
 
@@ -31,9 +32,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
-    
-    
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
     
 ## USING MIXINS ##
 
