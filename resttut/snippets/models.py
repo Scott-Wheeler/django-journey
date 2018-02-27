f663146e9eda2469e127e68ec456f31c37641828
Scott Wheeler <me@scottwheeler.ca>
2018-02-26 23:09:39 -0500

    Django REST - 5.1 - Creating an endpoint for the root of our API #146

--- resttut/snippets/models.py
+++ resttut/snippets/models.py
@@ -34,5 +34,5 @@ class Snippet(models.Model):
         options = self.title and {'title': self.title} or {}
         formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
         self.highlighted = highlight(self.code, lexer, formatter)
-        super(Snippet, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
         
